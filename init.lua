1f0cd0be40e4e2a66fcd14d8b43f4bf5fcd72aeb
Mark Pawson <mark@markpawson.co.uk>
2025-09-19 09:50:07 +0100

    updated syntax

--- init.lua
+++ init.lua
@@ -647,7 +647,7 @@ require('lazy').setup({
   },
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     require('lualine').setup()
   },
 --  {
