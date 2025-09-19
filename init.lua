f01cca4b659818030711f3c9e146e5a3b7d5a41f
Mark Pawson <mark@markpawson.co.uk>
2025-09-19 16:06:12 +0100

    updated syntax

--- init.lua
+++ init.lua
@@ -497,7 +497,7 @@ require('lazy').setup({
         texlab = {},    -- TeX
         bashls = {},    -- bash script
         gopls = {},     -- go
-        yang_lsp = {},  -- yang
+        yamlls = {},    -- yaml
         ansiblels = {}, -- ansible
         lua_ls = {      -- lua_ls: https://luals.github.io/wiki/settings/
           -- cmd = { ... },
